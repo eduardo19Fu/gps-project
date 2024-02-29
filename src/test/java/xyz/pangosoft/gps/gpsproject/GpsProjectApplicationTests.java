465807fa440d1c296ead0ad703e10155bebfbd78
Eduardo Fuentes <eduardo.fuentes@viaro.net>
2024-02-29 15:31:11 -0600

    Commit to add remote origin

---
+++ src/test/java/xyz/pangosoft/gps/gpsproject/GpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pangosoft.gps.gpsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
